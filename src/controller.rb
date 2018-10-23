34eb095ce16c1a7995646001638433088fab9edb
Your Name <you@example.com>
2018-10-23 01:26:47 -0200

    Created Json file search

--- src/controller.rb
+++ src/controller.rb
@@ -5,18 +5,21 @@ module Controller
 ##Request came in format: method path query
 ##Query start with '?' and is divide by '&' for each parameters
   def Controller.getUrlParams(request)
-    params = nil
+    params = {}
     method, full_path = request.split(' ')
     path, query = full_path.split('?')
     unless query.nil?
-     params = query.split('&')
+     query.split('&').each do |p|
+       key, value = p.split('=')
+       params[key] = value.tr('-', ' ')
+     end
     end
     return params
   end
 
 ##Build the response to client in HTTP protocol
-  def Controller.responseToClient(answer)
-    status, headers, body = app.call({})
+  def Controller.responseToClient(answer, session)
+    status, headers, body = answer.call({})
     session.print "HTTP/1.1 #{status}\r\n"
     headers.each do |key, value|
       session.print "#{key}: #{value}\r\n"
@@ -28,13 +31,15 @@ module Controller
   end
 
   def Controller.get(session, request)
-    params = Controller.getUrlParams(request)
+    params = getUrlParams(request)
+
+    result = Datasource.search(params)
 
     app = Proc.new do
-       ['200', {'Content-Type' => 'text/html'}, ["Hello world! #{params}"]]
+       ['200', {'Content-Type' => 'text/html'}, ["Hello world! #{result}"]]
      end
 
-    responseServer(app)
+    responseToClient(app, session)
 
   end
 end
--- src/datasource.rb
+++ src/datasource.rb
@@ -1,17 +1,49 @@
 
 require 'json'
 
+
+
 module Datasource
 
-  data_hash = {}
+
 
   #Read the Json file and save the data in a Hash
   def Datasource.init
+    @data_hash = {}
     print("---Reading Json file \n")
     file = File.read('books.json')
-    data_hash = JSON.parse(file)
+    @data_hash = JSON.parse(file)
 
     print("Successfully \n")
+  end
+
+
+  def Datasource.search(params)
+    answer = @data_hash
+
+    unless params["Name"].nil?
+      answer.select! { | book | book["name"].include? params["Name"]}
+      params.delete("Name")
+    end
+
+    unless params["Sort"].nil?
+      sort = params["Sort"]
+      params.delete("Sort")
+    end
+
+    params.each do |key, value|
+      current_key = key.tr('-', ' ').capitalize
+      next unless answer.first["specifications"].has_key?(current_key.to_s)
+      answer.select! { | book | book["specifications"][current_key].include? value}
+    end
+
+    if sort == "asc"
+      return answer.sort_by { | h | h["price"]}
+    elsif sort == "desc"
+       return answer.sort_by { | h | -h["price"]}
+    end
+
+    return answer
   end
 
 end
