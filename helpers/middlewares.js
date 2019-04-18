37d9c76a0a5fffcbf1ba73d2d71b605ef9bd38da
Roberto Aoki <nogueiraaoki@gmail.com>
2019-04-18 07:28:21 -0300

    adicionando helpers para i/o json

---
+++ helpers/middlewares.js
@@ -0,0 +1,24 @@
+function mustBeInteger(req, res, next) {
+    const id = req.params.id
+
+    if (!Number.isInteger(parseInt(id))) {
+        res.status(400).json({ message: 'ID must be an integer' })
+    } else {
+        next()
+    }
+}
+
+function checkFieldsPost(req, res, next) {
+    const { name, price } = req.body
+
+    if (name && price) {
+        next()
+    } else {
+        res.status(400).json({ message: 'fields are not good' })
+    }
+}
+
+module.exports = {
+    mustBeInteger,
+    checkFieldsPost
+}
