ad8df5f5196a6391b4b87bb6f219f0b0320874e3
Roberto Aoki <nogueiraaoki@gmail.com>
2019-04-18 07:30:22 -0300

    atualizando rotas em index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const morgan = require('morgan')
 const app = express()
 // Morgan
 app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(require('./routes/index.routes'))
 // First route
 app.get('/', (req, res) => {
     res.json({ message: 'Hello world' })
