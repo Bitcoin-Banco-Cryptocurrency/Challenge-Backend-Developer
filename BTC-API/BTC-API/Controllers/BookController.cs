6d8542a134a2c8ba32771f949a488da69784ad76
dmm2018 <36994408+dmm2018@users.noreply.github.com>
2019-05-10 02:27:53 -0300

    BTC-API
    Open API in visual studio and run before WEB

---
+++ BTC-API/BTC-API/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BTC_API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTC_API.Controllers
+{
+    public class BookController : Controller
+    {
+        [HttpGet]
+        [Route("api/book")]
+        public IActionResult Get()
+        {
+            var result = new BookService().GetAll();
+            return Ok(new {success = true, data = result });
+        }
+    }
+}
---
+++ BTC-API/BTC-API/Models/Specification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTC_API.Models
+{
+    public class Specification
+    {
+        public int Id { get; set; }
+
+        public string Published { get; set; }
+
+        public string Author { get; set; }
+
+        public int PageCount { get; set; }
+
+        public string Illustrator { get; set; }
+
+        public virtual List<Genre> Genres { get; set; }
+    }
+}
