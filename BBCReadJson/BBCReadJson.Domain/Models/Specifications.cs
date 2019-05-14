2d8d008079e1f521053dd935fe5f2233b3c936dc
Vinicius Graeser <viniciusgraeser@outlook.com.br>
2019-05-14 11:11:13 -0300

    Leitura do arquivo json.

--- BBCReadJson/BBCReadJson.Domain/Models/Specifications.cs
+++ BBCReadJson/BBCReadJson.Domain/Models/Specifications.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace BBCReadJson.Domain.Models
 {
     public class Specifications
     {
+        public Specifications()
+        {
+            IllustratorList = new List<string>();
+            GenresList = new List<string>();
+        }
+
         [JsonProperty("Originally published")] public string OriginallyPublished { get; set; }
 
         [JsonProperty("Author")] public string Author { get; set; }
 
         [JsonProperty("Page count")] public int PageCount { get; set; }
 
-        [JsonProperty("Illustrator")] public IEnumerable<string> IllustratorList { get; set; }
+        [JsonProperty("Illustrator")] public object IllustratorObject { get; set; }
 
-        [JsonProperty("Genres")] public IEnumerable<string> GenresList { get; set; }
+        public List<string> IllustratorList { get; set; }
+
+        [JsonProperty("Genres")] public object GenresObject { get; set; }
+
+        public List<string> GenresList { get; set; }
     }
 }
--- BBCReadJson/BBCReadJson.Infra.Data/Repositories/BookRepository.cs
+++ BBCReadJson/BBCReadJson.Infra.Data/Repositories/BookRepository.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using BBCReadJson.Domain.Interfaces;
 using BBCReadJson.Domain.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BBCReadJson.Infra.Data.Repositories
 {
@@ -16,6 +20,23 @@ namespace BBCReadJson.Infra.Data.Repositories
             var json = File.ReadAllText(path);
 
             var books = JsonConvert.DeserializeObject<IEnumerable<Book>>(json);
+
+            foreach (var item in books)
+            {
+                var typeIllustratorObject = item.Specifications.IllustratorObject.GetType();
+
+                if (typeIllustratorObject.Name.Equals("JArray"))
+                    item.Specifications.IllustratorList.AddRange(JsonConvert.DeserializeObject<List<string>>(item.Specifications.IllustratorObject.ToString()));
+                else
+                    item.Specifications.IllustratorList.Add(item.Specifications.IllustratorObject.ToString());
+
+                var typeGenresObject = item.Specifications.GenresObject.GetType();
+
+                if (typeGenresObject.Name.Equals("JArray"))
+                    item.Specifications.GenresList.AddRange(JsonConvert.DeserializeObject<List<string>>(item.Specifications.GenresObject.ToString()));
+                else
+                    item.Specifications.GenresList.Add(item.Specifications.GenresObject.ToString());
+            }
 
             return books;
         }
